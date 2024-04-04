b84cfb26705a5940dfb1a4dac310d142affc0f9a
HPReyes <hpreyesdev@gmail.com>
2024-04-04 03:34:58 +0200

    first commit

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-function App() {
-
-  return (
-    <p className="text-red-500 text-6xl">Hola</p>
-  )
-}
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {RouterProvider} from 'react-router-dom'
+import { QuioscoProvider } from './context/QuioscoProvider'
+import router from './router'
 import './index.css'
+// import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QuioscoProvider>
+    <RouterProvider router={router} />
+    </QuioscoProvider>
   </React.StrictMode>,
 )
